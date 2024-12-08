eb73bd59a3082f5da4357b85ea6f9b3d6d57dc2d
Swenschaeferjohann <swen-code@swens-MacBook-Air-2.local>
2024-12-08 01:28:50 +0000

    add MIT

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+/// Adapted from https://github.com/jup-ag/jupiter-quote-api-node/blob/main/
+/// MIT License
+/// For purpose of wrapping the Jupiter API with compressed token support.
 export * from './DefaultApiAdapter.ts';
 export * from './defaultConfigs.ts';
 export * from './instructions.ts';
