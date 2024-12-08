4bae7a2b85f09e8e638ae73a73a7bb2de240f4a4
Swenschaeferjohann <swen-code@swens-MacBook-Air-2.local>
2024-12-08 02:21:55 +0000

    minimify +add sourcemaps for beta

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,4 +5,15 @@ export default defineConfig({
     format: ['cjs', 'esm'],
     dts: true,
     clean: true,
+    minify: true,
+    external: [
+        '@jup-ag/api',
+        '@lightprotocol/compressed-token',
+        '@lightprotocol/stateless.js',
+        '@solana/spl-token',
+        '@solana/web3.js',
+        'bs58'
+    ],
+    treeshake: true,
+    sourcemap: true,
 });
